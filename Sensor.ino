8fef1af7974c9cf905b5bd471ff7472078c909a5
Rakesh Shrestha <56125560+UnknownBeginner3222@users.noreply.github.com>
2020-04-28 23:33:56 +0545

    Add files via upload

---
+++ Sensor.ino
@@ -0,0 +1,30 @@
+
+int buzzer = 10;
+int smokeA0 = A5;
+
+// Your threshold value. You might need to change it.
+int sensorThres = 200;
+
+void setup() {
+  pinMode(buzzer, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThres)
+  {
+    tone(buzzer, HIGH); // Can be placed as HIGH, LOW i.e 1000, 200
+  
+  }
+  else
+  {
+    noTone(buzzer);
+  }
+  delay(100);
+}
